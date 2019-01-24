d7ef10307f841bd7e8ba18753adae8b7c2d5d4d9
Chris Wilcoxson <cwilcoxson@churchcommunitybuilder.com>
2019-01-24 16:01:50 -0700

    Fetch forms from Firebase and display on the YourForms page.

--- src/components/ViewForm.js
+++ src/components/ViewForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled, { ThemeProvider } from 'styled-components';
 import moment from 'moment';
+import * as R from 'ramda';
+import { fields } from '../constants/fields';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme } from '@material-ui/core/styles';
@@ -51,15 +53,37 @@ const StyledTableRow = styled(TableRow)`
   }
 `;
 
+const sortByDisplayOrder = R.sortWith([
+  R.ascend(R.prop('displayOrder')),
+]);
+
+const maybeFormatDate = (fieldData, fieldKey) => {
+  if (fieldKey.startsWith('date')) {
+    return moment( fieldData, 'YYYY-MM-DD' ).format( 'M/D/YYYY' )
+  }
+  return fieldData;
+}
+
 export class ViewForm extends Component {
   render() {
     const { form } = this.props;
+    const hasData = R.filter( R.compose( R.not, R.isEmpty), form );
+    let fieldData = Object.entries(hasData);
+    const removeTheseFields = ['dateSubmitted','formId','formName','submitterId'];
+    fieldData = fieldData.filter( data => {
+      return ! removeTheseFields.includes(data[0]) 
+    });
+    fieldData.forEach(function(data){
+      const fieldInfo = R.find(R.propEq('key', data[0]))(fields);
+      data.displayOrder = fieldInfo.displayOrder;
+    });
+    fieldData = sortByDisplayOrder( fieldData );
     return (
       <ThemeProvider theme={theme}>
         <Main>
           <CssBaseline />
           <Grid item xs={ 12 }>
-            <Typography component="h1" paragraph variant="title">{ form.formName }</Typography>
+            <Typography component="h1" paragraph variant="title">{ hasData.formName }</Typography>
 
             <Typography component="h2" paragraph>Form Fields</Typography>
             <StyledTable>
@@ -71,12 +95,15 @@ export class ViewForm extends Component {
               </StyledTableHead>
               <TableBody>
                 {
-                  form.fields.map((field,i) => (
-                    <StyledTableRow key={ i }>
-                      <TableCell>{ field.fieldName }</TableCell>
-                      <TableCell>{ field.value }</TableCell>
-                    </StyledTableRow>
-                  ))
+                  fieldData.map((field,i) => {
+                    const fieldInfo = R.find(R.propEq('key', field[0]))(fields);
+                    return (
+                      <StyledTableRow key={ i }>
+                        <TableCell>{ fieldInfo.label }</TableCell>
+                        <TableCell>{ maybeFormatDate( field[1], field[0] ) }</TableCell>
+                      </StyledTableRow>
+                    )
+                  })
                 }
               </TableBody>
             </StyledTable>
@@ -94,7 +121,7 @@ export class ViewForm extends Component {
                   <TableCell>Date Submitted</TableCell>
                   <TableCell>{ moment( form.dateSubmitted, 'YYYY-MM-DDTHH:mm' ).format( 'M/D/YYYY h:mm A' ) }</TableCell>
                 </StyledTableRow>
-                {
+                {/*
                   form.approvals.map((approval,i) => {
                     return(
                     <StyledTableRow key={ i }>
@@ -102,7 +129,7 @@ export class ViewForm extends Component {
                       <TableCell>{ moment( approval.timestamp, 'YYYY-MM-DDTHH:mm' ).format( 'M/D/YYYY h:mm A' ) }</TableCell>
                     </StyledTableRow>
                   )})
-                }
+                    */}
               </TableBody>
             </StyledTable>
           </Grid>
