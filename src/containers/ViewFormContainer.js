763027bfbe911f8cd634e2ba2e8378f544964609
Chris Wilcoxson <cwilcoxson@churchcommunitybuilder.com>
2019-03-07 16:38:56 -0700

    Move approve or deny methods to FormActions component.

--- src/containers/ViewFormContainer.js
+++ src/containers/ViewFormContainer.js
@@ -8,32 +8,6 @@ import { convertToArrayWithFormId } from '../functions';
 import ViewForm from '../components/ViewForm/ViewForm';
 
 class ViewFormContainer extends React.Component {
-  handleApproval = (row) => (event) => {
-    const currentUser = this.props.authUser;
-    console.log({row,currentUser});
-    const docRef = this.props.firebase.db.collection('forms').doc(row.formId);
-
-    // who should be next in the chain?
-    // should this approval message then show "waiting on {next person}"?
-
-    docRef.update({formStatus: row.formStatus + 1});
-    docRef.update({formStatusBy: currentUser.uid});
-
-    // write to Firestore on the form object
-    // add an approval in the approvals array
-    // change status to waiting for ...next role
-  }
-
-  handleDenial = (row) => ( event ) => {
-    const currentUser = this.props.authUser;
-    const docRef = this.props.firebase.db.collection('forms').doc(row.formId);
-    // what else should happen here?
-    docRef.update({formStatus: 'Denied'});
-    docRef.update({formStatusBy: currentUser.uid});
-    // write to Firestore
-    // change status to denied
-  }
-  
   render() {
     if ( ! isLoaded( this.props.forms ) ) {
       return null;
