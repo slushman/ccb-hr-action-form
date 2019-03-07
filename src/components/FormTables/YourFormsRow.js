4e3f57e55e1f0b07d13f15facc45c874df8eee26
Chris Wilcoxson <cwilcoxson@churchcommunitybuilder.com>
2019-03-07 16:40:42 -0700

    Add new status component.

--- src/components/FormTables/YourFormsRow.js
+++ src/components/FormTables/YourFormsRow.js
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
+import styled from 'styled-components';
 
+import StatusItem from './StatusItem';
 import {
   TableCell,
   TableRow,
 } from '../../styles';
+
+const StatusList = styled.ul`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 
 class YourFormsRow extends React.Component {
 
@@ -34,7 +43,15 @@ class YourFormsRow extends React.Component {
         </TableCell>
         <TableCell align="right">{row.requestType}</TableCell>
         <TableCell align="right">{ dayjs( row.dateSubmitted, 'YYYY-MM-DDTHH:mm:ss' ).format( 'M/D/YYYY h:mm A' ) }</TableCell>
-        <TableCell align="right">{ 4 > row.formStatus ? `${row.formStatus}/4` : 'Approved'}</TableCell>
+        <TableCell align="right">
+            <StatusList>
+              { row.responses && 
+                Object.entries(row.responses).map( ( response, i ) => (
+                  <StatusItem key={ i } label={ response[0] } responseInfo={ response[1] } />
+                ) )
+              }
+            </StatusList>
+        </TableCell>
       </TableRow>
     );
   }
--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,16 @@
+import React from 'react';
+
 export const convertToArrayWithFormId = ( forms ) => {
   return Object.entries( forms ).map( ( form ) => {
     return { ...form[1], formId: form[0] };
   } );
+}
+
+export const getStatus = ( responseInfo ) => {
+  if ( 'approved' === responseInfo.response ) {
+    return <span aria-label="approved" role="img">✅</span>;
+  } else if ( 'denied' === responseInfo.response ) {
+    return <span aria-label="denied" role="img">❌</span>;
+  }
+  return <span aria-label="not yet" role="img">¯\_(ツ)_/¯</span>;
 }
