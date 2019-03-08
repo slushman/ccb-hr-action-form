c5f81b57ed748bea56de95a792a1b53279204f89
Chris Wilcoxson <cwilcoxson@churchcommunitybuilder.com>
2019-03-08 16:05:32 -0700

    Remove current user from LT approvals select options.

--- src/components/NewForm/ApprovalFields.js
+++ src/components/NewForm/ApprovalFields.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as R from 'ramda';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
+
 import Select from '../Fields/Select';
 
 import {
@@ -7,6 +12,29 @@ import {
   Legend,
 } from '../../styles';
 import * as EMAILS from '../../constants/emails';
+
+const approvalOptions = [
+  {
+    label: 'Aaron Senneff',
+    value: EMAILS.AARONSENNEFF,
+  },
+  {
+    label: 'Amanda Williams',
+    value: EMAILS.AMANDAWILLIAMS,
+  },
+  {
+    label: 'Heather Sharp',
+    value: EMAILS.HEATHERSHARP,
+  },
+  {
+    label: 'Jeff Otero',
+    value: EMAILS.JEFFOTERO,
+  },
+  {
+    label: 'Chris Wilcoxson',
+    value: 'cwilcoxson@gmail.com',
+  },
+];
 
 /**
  * Field group appears for all fields, if requestType has a value.
@@ -17,34 +45,17 @@ class ApprovalFields extends React.Component {
   };
 
   render() {
+
+    const removeMeFromOptions = (option) => this.props.authUser.email !== option.value; // Don't match the current user email.
+    let filteredOptions = R.filter( removeMeFromOptions, approvalOptions ); // Filter out the current user.
+
     return (
       <Fieldset>
         <Legend>Approvals Needed</Legend>
         <Select
           label="Leadership Team"
           name="responses.LT.contact"
-          options={[
-            {
-              label: 'Aaron Senneff',
-              value: EMAILS.AARONSENNEFF,
-            },
-            {
-              label: 'Amanda Williams',
-              value: EMAILS.AMANDAWILLIAMS,
-            },
-            {
-              label: 'Heather Sharp',
-              value: EMAILS.HEATHERSHARP,
-            },
-            {
-              label: 'Jeff Otero',
-              value: EMAILS.JEFFOTERO,
-            },
-            {
-              label: 'Chris Wilcoxson',
-              value: 'cwilcoxson@gmail.com',
-            },
-          ]}
+          options={ filteredOptions }
           placeholder="Select leadership team member"
         />
         {/* { ( 'employment' === values.requestType &&
@@ -71,4 +82,15 @@ class ApprovalFields extends React.Component {
   }
 }
 
-export default ApprovalFields;
+const mapStateToProps = ( state ) => {
+  return {
+    authUser: state.firebase.auth,
+  };
+};
+
+const enhance = compose(
+  withFirebase,
+  connect( mapStateToProps )
+);
+
+export default enhance( ApprovalFields );
